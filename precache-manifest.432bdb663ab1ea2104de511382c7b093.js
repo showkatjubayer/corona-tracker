644fdb4c2fb7afdd5daf01db20f4f18b4e9955be
Showkat Jamil Jubayer <62599772+showkatjubayer@users.noreply.github.com>
2020-10-05 12:49:05 +0600

    Add files via upload

---
+++ precache-manifest.432bdb663ab1ea2104de511382c7b093.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10be74ca6898c1e80079edb4c30b289b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "397a2b21d1f595000cd8",
+    "url": "/static/css/main.9bccf2ff.chunk.css"
+  },
+  {
+    "revision": "1505c8ba820279735f85",
+    "url": "/static/js/2.2fa7b665.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.2fa7b665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "397a2b21d1f595000cd8",
+    "url": "/static/js/main.d1bbd594.chunk.js"
+  },
+  {
+    "revision": "717a8abd0f249375b28a",
+    "url": "/static/js/runtime-main.0dea4190.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
